3ba9d9cac9f68f3b2edc0c39b997ca102191e111
Vignesh Babu B R B <vigneshbabu.brb@zohomail.in>
2021-09-13 21:46:10 +0530

    Initial Commit

---
+++ src/main/java/com/vb/javarestfulweb/JavaRestfulWebApplication.java
@@ -0,0 +1,13 @@
+package com.vb.javarestfulweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestfulWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRestfulWebApplication.class, args);
+    }
+
+}
